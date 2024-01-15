6e9be7e33bb4127efd47f7918e9093179e7fd767
shourovr82 <shafinur512@gmail.com>
2024-01-15 20:42:52 +0600

    [DONE] get all orders module completed without filters

--- src/app/modules/foodOrders/orders.constants.ts
+++ src/app/modules/foodOrders/orders.constants.ts
@@ -1,9 +1,9 @@
-export const foodItemFilterableFields: string[] = ["searchTerm", "role", "userStatus"];
+export const ordersFilterableFields: string[] = ["searchTerm", "role", "userStatus"];
 
-export const foodItemSearchableFields: string[] = ["firstName", "lastName", "phoneNumber", "user"];
+export const ordersSearchableFields: string[] = ["firstName", "lastName", "phoneNumber", "user"];
 
-export const foodItemRelationalFields: string[] = ["userStatus", "role"];
-export const foodItemRelationalFieldsMapper: { [key: string]: string } = {
+export const ordersRelationalFields: string[] = ["userStatus", "role"];
+export const ordersRelationalFieldsMapper: { [key: string]: string } = {
   userStatus: "userStatus",
   role: "role",
 };
